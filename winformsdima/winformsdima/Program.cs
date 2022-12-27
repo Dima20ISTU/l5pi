20a77a1f665c709355863269a0fb283e4193ba15
Dima <Psarev20@mail.ru>
2022-12-27 15:11:26 +0300

    Добавлены комментарии к классу Program

--- winformsdima/winformsdima/Program.cs
+++ winformsdima/winformsdima/Program.cs
@@ -16,7 +16,7 @@ namespace winformsdima
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new MainForm()); // 23234334234423
         }
     }
 }
